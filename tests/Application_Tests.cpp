b1f4ff76bd252e183d8266b5bf60756e7b36ca52
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2024-02-18 15:14:53 +0100

    Test fundaments of Application class

---
+++ tests/Application_Tests.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <signal.h>
+
+#include "Application.hpp"
+
+TEST(Application, WindowCreated)
+{
+	volatile Application app;
+
+	ASSERT_NE(app.Instance().Window(), nullptr) << "Window was not created as it should.";
+}
+
+TEST(Application, MultipleAppGuard)
+{
+	volatile Application app1;
+	
+	ASSERT_DEATH({ volatile Application app2; }, "Only one instance of Application allowed!");
+}
+
+TEST(Application, SequentialAppCreation)
+{
+	{
+		volatile Application app1;
+	}
+
+	ASSERT_NO_THROW(volatile Application app2) << "Application should not throw if the first instance is already dead.";
+}
--- tests/Filled.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(xd, xd1)
-{
-	ASSERT_TRUE(true);
-}
