fdade9805674facf187e82b4d62a9d0e7a7fa485
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2024-02-20 20:04:15 +0100

    Playground

--- src/layers/EditorLayer.cpp
+++ src/layers/EditorLayer.cpp
@@ -19,7 +19,7 @@ EditorLayer::EditorLayer()
 	dummyEv.Size.Height = spec.Height;
 
 	m_EditorCamera.OnEvent(dummyEv);
-	m_EditorCamera.m_Position = { 0.0f, 10.0f, 20.0f };
+	m_EditorCamera.Position = { 0.0f, 10.0f, 20.0f };
 }
 
 void EditorLayer::OnAttach()
@@ -62,12 +62,27 @@ void EditorLayer::OnTick()
 {
 }
 
+static glm::vec3 s_LightPos = glm::vec3(0.0f);
+static glm::vec3 s_LightCol = glm::vec3(1.0f);
+static float s_Linear    = 0.022f;
+static float s_Quadratic = 0.0019f;
+
 void EditorLayer::OnRender()
 {
 	Renderer::Clear();
 	Renderer::ClearColor({ 0.3f, 0.4f, 0.5f, 1.0f });
 
 	Renderer::SceneBegin(m_EditorCamera);
+	Renderer::AddPointLight({
+		.Position = s_LightPos,
+		.Color = s_LightCol,
+		.LinearTerm = s_Linear,
+		.QuadraticTerm = s_Quadratic
+		});
+	
+	Renderer::DrawCube(s_LightPos, glm::vec3(0.1f), glm::vec4(s_LightCol, 1.0f));
+	Renderer::DrawCube(glm::vec3(0.0f), { 20.0f, 0.2f, 20.0f }, { 1.0f, 1.0f, 0.0f, 1.0f });
+
 	Renderer::DrawQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f, 1.0f }, { 1.0f, 0.0f, 0.0f, 1.0f });
 
 	Renderer::DrawLine({  10.0f, 0.0f,  10.0f }, { -10.0f, 0.0f,  10.0f }, { 1.0f, 0.0f, 0.0f, 1.0f });
@@ -89,9 +104,17 @@ void EditorLayer::OnRender()
 	ImGui::Begin("Hiii!!!!!!");
 	if (ImGui::CollapsingHeader("Camera"))
 	{
-		ImGui::DragFloat3("Position", glm::value_ptr(m_EditorCamera.m_Position), 1.0f, -FLT_MAX, FLT_MAX);
+		ImGui::DragFloat3("Cam position", glm::value_ptr(m_EditorCamera.Position), 1.0f, -FLT_MAX, FLT_MAX);
 		ImGui::DragFloat("Pitch", &m_EditorCamera.m_Pitch, 1.0f, -FLT_MAX, FLT_MAX);
 		ImGui::DragFloat("Yaw", &m_EditorCamera.m_Yaw, 1.0f, -FLT_MAX, FLT_MAX);
+	}
+
+	if (ImGui::CollapsingHeader("Point light source"))
+	{
+		ImGui::DragFloat3("Light position", glm::value_ptr(s_LightPos), 0.05f, -FLT_MAX, FLT_MAX);
+		ImGui::DragFloat3("Color", glm::value_ptr(s_LightCol), 0.01f, 0.0f, 1.0f);
+		ImGui::DragFloat("Linear term", &s_Linear, 0.001f, 0.0f, 1.0f);
+		ImGui::DragFloat("Quadratic term", &s_Quadratic, 0.001f, 0.0f, 2.0f);
 	}
 	ImGui::End();
 }
