1d8f579f18b46f3764a7ecd2f43bbd213d29b267
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2024-03-09 13:23:07 +0100

    lol

--- src/scenes/Components.cpp
+++ src/scenes/Components.cpp
@@ -7,6 +7,6 @@
 glm::mat4 TransformComponent::ToMat4() const
 {
 	return glm::translate(glm::mat4(1.0f), Position)
-		* glm::scale(glm::mat4(1.0f), Scale)
-		* glm::toMat4(glm::quat(Rotation));
+		* glm::toMat4(glm::quat(Rotation))
+		* glm::scale(glm::mat4(1.0f), Scale);
 }
